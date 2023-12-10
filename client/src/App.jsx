495d2f819397526ce30f8edb448a4f980bca76d2
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-12-10 17:35:39 +0200

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
+import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 
 function App() {
 
   return (
-   <>
-   < Header />
-   </>
+    <>
+      < Header />
+
+      <Footer />
+    </>
   )
 }
 
