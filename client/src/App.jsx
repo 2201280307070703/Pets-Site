53d86c08ef40a78cb826d08fd4f50385af1c258d
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2023-12-10 17:38:55 +0200

    add Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import Footer from "./components/footer/Footer"
-import Header from "./components/header/Header"
+import {Routes, Route} from 'react-router-dom';
+
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 function App() {
 
   return (
     <>
       < Header />
+      < Routes>
+        
+      </Routes>
 
       <Footer />
     </>
